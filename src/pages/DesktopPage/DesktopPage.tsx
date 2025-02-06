36587ea4cbb2e1ab21cb84350726dcc6b215f8b4
alexandr-obraztsov <alexlove2005@mail.ru>
2025-02-06 12:02:49 +0300

    add hello animation

--- src/pages/DesktopPage/DesktopPage.tsx
+++ src/pages/DesktopPage/DesktopPage.tsx
@@ -1,3 +1,6 @@
+import helloAnimation from '@/assets/animations/hello.json'
+import { useLottie } from 'lottie-react'
+import { useEffect, useState } from 'react'
 import { WidgetsContainer } from '../WidgetsContainer/WidgetsContainer'
 import { BottomMenu } from './BottomMenu/BottomMenu'
 import { Desktop } from './Desktop/Desktop'
@@ -5,14 +8,32 @@ import s from './DesktopPage.module.css'
 import { Header } from './Header/Header'
 
 export const DesktopPage = () => {
+	const [isLoading, setIsLoading] = useState(true)
+	const { View, play } = useLottie({
+		animationData: helloAnimation,
+		loop: false,
+		autoplay: false,
+	})
+
+	useEffect(() => {
+		setTimeout(() => {
+			play()
+		}, 500)
+
+		setTimeout(() => {
+			setIsLoading(false)
+		}, 5000)
+	}, [play, setIsLoading])
+
 	return (
-		<div className={s.desktopContainer}>
-			<div className={s.desktop}>
+		<div className={s.desktopContainer + ' ' + (isLoading && s.hidden)}>
+			<div className={s.desktop + ' ' + (isLoading && s.hidden)}>
 				<Header />
 				<Desktop />
 				<BottomMenu />
 				<WidgetsContainer />
 			</div>
+			<div className={s.loading + ' ' + (!isLoading && s.hidden)}>{View}</div>
 		</div>
 	)
 }
